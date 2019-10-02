3ecc619f02ceedb4f742c5412d4d91d65fa400fe
Jasmin Ramirez <jasmin@jasmins-macbook-pro.local>
2019-10-01 22:43:12 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Jasmin Ramirez  on 10/1/19.
+//  Copyright © 2019 Jasmin Ramirez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
